070bf16959c9bcce9ad9d0fb5de2f568d659f180
Prithvi1122 <65720342+Prithvi1122@users.noreply.github.com>
2020-07-07 19:56:41 +0530

    on 7th July

--- sketch.js
+++ sketch.js
@@ -1,17 +1,27 @@
-var canvas,gameState=0,playerCount,database,form,player,game;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
 var allPlayers;
-var distance=0;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+
 function setup(){
-	canvas=createCanvas(500,500);
-	database=firebase.database();
-	game=new Game();
-	game.getState();
-	game.start();
-
-
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
+
 function draw(){
-if(playerCount === 4){
+  if(playerCount === 4){
     game.update(1);
   }
   if(gameState === 1){
